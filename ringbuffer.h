642a3d5ae325fe058768a60b3171f0533798f4fe
Jaketsuki <jaakko.keko@gmail.com>
2017-10-09 10:34:19 +0300

    Update ringbuffer.h

--- ringbuffer.h
+++ ringbuffer.h
@@ -1,48 +1,24 @@
-#include <assert.h>
+#define MAX_BUFFER 10
+#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <gtest/gtest.h>
 
-#define MAX_BUFFER 10 ///< Length of global buffer space
-
-#define OK 1     ///< OK
-#define ERROR -1 ///< Error
-
-#define BUFF_LEN(a, b) a < b ? b - a : a - b
-
-//! \enum error_type enumerators for error conditions
-enum error_type {
-  BUFF_OK,        /*! No error */
-  BUFFER_EMPTY,   /*! Buffer is empty */
-  BUFFER_FULL,    /*! Buffer is full */
-  BUFFER_OVERRUN, /*! Try to read over buffer end */
-  BUFFER_ERROR,
-  UNKNOWN_ERROR
+struct buffer_type
+{
+	uint8_t *head;
+	uint8_t *tail;
+	uint8_t *buffer;
 };
+enum {OK, BUFFER_EMPTY, BUFFER_OVERRUN, BUFFER_HEAD_AT_TAIL, BUFFER_OUT, BUFFER_HEAD_END};
 
-/*
-\struct buffer_type
-*/
-struct buffer_type {
-  unsigned char *head;   ///< Current adding position of the buffer
-  unsigned char *tail;   ///< Currently last entry
-  unsigned char *buffer; ///< Pointer to buffer
-};
 
+
+void init_buffer(struct buffer_type *b, uint8_t *buffer);
 void empty_buffer(struct buffer_type *b);
-void init_buffer(struct buffer_type *b, unsigned char *buffer);
-enum error_type create_buffer(struct buffer_type *b, int size);
-int add_char_to_buffer(struct buffer_type *b, unsigned char c, enum error_type *err);
-unsigned char get_char_from_buffer(struct buffer_type *b, enum error_type *err);
-int add_string_to_buffer(struct buffer_type *b, unsigned char *s, enum error_type *err);
-unsigned char get_string_from_buffer(struct buffer_type *b, unsigned char *s,
-                            enum error_type *err);
-int get_buffer_state(struct buffer_type b);
-void print_buffer(struct buffer_type b);
-unsigned char *move_pointer_to_next(struct buffer_type *b, unsigned char *p);
-
-enum error_type is_buffer_full(struct buffer_type *b);
-enum error_type is_buffer_empty(struct buffer_type *b);
-int get_buffer_length(struct buffer_type *b);
-
-extern unsigned char transmission_buffer[MAX_BUFFER];
-extern unsigned char recive_buffer[MAX_BUFFER];
+void make_buffer(uint8_t message[]);
+int16_t get_buffer_status(struct buffer_type b, int16_t *err);
+int16_t add_char_to_buffer(struct buffer_type *b, uint8_t c, int16_t *err);
+uint8_t get_char_from_buffer(struct buffer_type *b, int16_t *err);
+int16_t print16_t_buffer(struct buffer_type b, int16_t *err);
+int16_t add_string_to_buffer(struct buffer_type *b, uint8_t *s, int16_t *err);
+int16_t  get_string_from_buffer(struct buffer_type *b, uint8_t *dest, int16_t len, int16_t *err);
